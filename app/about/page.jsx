0c5556a2edc62a3fd96b584423a71817c827a286
Ophélie Sacré <ophelie.sacre@outlook.be>
2026-02-09 16:25:59 +0100

    update

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,7 +4,11 @@ export default function AboutPage()
 {
     return(
         <>
-            
+            <div className="w-screen h-screen flex items-center justify-center">
+                <div>
+                    <h1 className="text-fuchsia-800 text-2xl">About Page</h1>
+                </div>
+            </div>
         </>
     )
 }
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,11 @@ export default function ContactPage()
 {
     return(
         <>
-            
+            <div className="w-screen h-screen flex items-center justify-center">
+                <div>
+                    <h1 className="text-fuchsia-800 text-2xl">Contact Page</h1>
+                </div>
+            </div>
         </>
     )
 }
